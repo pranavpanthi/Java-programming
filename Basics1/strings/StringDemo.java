fd84ce0604ba1e58612643643ed8c292d8fc10ce
Prahlad Panthi <prahladpanthi@Prahlads-MacBook-Pro.local>
2024-05-12 17:53:50 +1000

    demo of string class

---
+++ Basics1/strings/StringDemo.java
@@ -0,0 +1,21 @@
+// package strings;
+
+import java.util.Scanner;
+
+public class StringDemo {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.println(name.equals("Pranav"));
+        System.out.println(name.equalsIgnoreCase("Pranav")); // pranav PRANAV pRaNaV
+
+        // method chaining
+        boolean isPranav = sc.next().equalsIgnoreCase("Pranav");
+        System.out.println("Is Pranav? : " + isPranav);
+    }
+    
+}
