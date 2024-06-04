7a3e1ecdffe219abf20de7f8e702730ba870b01e
Prahlad Panthi <prahladpanthi@Prahlads-MacBook-Pro.local>
2024-06-04 22:50:46 +1000

    added demo of getter-setter in Student.java

---
+++ Inheritance/simple/Student.java
@@ -0,0 +1,25 @@
+public class Student {
+    // name, roll number, department, gender, address
+
+    private String name;
+
+    private int rollNumber;
+
+    private String department;
+
+    private String gender;
+
+    private String address;
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getRollNumber() { 
+        return rollNumber;
+    }
+}
