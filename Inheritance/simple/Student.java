ccceb57b4716c5f64b6573357bbdbd7388ec1fcf
Pranav Panthi <pathipranav@gmail.com>
2024-06-04 18:49:12 +0545

    Added Getters and Setters

--- Inheritance/simple/Student.java
+++ Inheritance/simple/Student.java
@@ -22,4 +22,32 @@ public class Student {
     public int getRollNumber() { 
         return rollNumber;
     }
+
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    } 
+    
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
