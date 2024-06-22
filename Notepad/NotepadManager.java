81cd868335795893aa98a316631ea122067b2263
Pranav Panthi <pathipranav@gmail.com>
2024-06-22 16:07:13 +0545

    Other COde will be updated when Features are Completed

---
+++ Notepad/NotepadManager.java
@@ -0,0 +1,22 @@
+import java.io.*;
+
+public class NotepadManager {
+    public static void main(String[] args) {
+        try {
+            FileWriter fw = new FileWriter("Notepad.txt");
+            BufferedWriter bfw = new BufferedWriter(fw);
+            String str1 = "---------------WELCOME !!!-------------\n";
+            bfw.write(str1);
+            String str2 = "------------------TO-------------------\n";
+            bfw.write(str2);
+            String str3 = "---------------NOTEPAD !!!-------------\n";
+            bfw.write(str3);
+            
+            System.out.println("File Written");
+            bfw.close();
+
+        } catch (Exception e) {
+            System.out.println("AN SYSTEM ERROE HAS OCCURED!!!");
+        }
+    }
+}
